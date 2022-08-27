ec850a11894c78792b449182951405770936b15a
Adel <80000156+Cyberft-pdf@users.noreply.github.com>
2022-08-27 23:56:57 +0200

    Add files via upload

---
+++ mathapp.py
@@ -0,0 +1,70 @@
+
+import random 
+
+
+q1 = input("Jaký je objem kvádru?:")
+if q1 == "abc":
+    print("správně")
+
+else:
+    print("špatně")
+
+q2= input("Jaký je povrch kvádru?:")
+if q2 == "2 * (ab+ac+cb)":
+    print("správně")
+
+else : 
+    print("špatně")
+
+q3 = input("jaký je objem hranolu?:")
+if q3 == "2 * ab + 2 (a + b) v":
+    print("správně")
+else :
+    print("špatně")
+
+q4 = input("jaký je povrch hranolu?:")
+if q4 == "2 * Sp + Spl":
+    print("správně")
+
+else : 
+    print("špatně")
+
+q5 = input("Jaký je objem krychle?:")
+if q5 == "a3":
+    print("správně")
+else:
+    print("špatně")
+
+q6 = input("Jaký je povrch krychle?:")
+if q6 == "6a2":
+    print("správně")
+else:
+    print("špatně")
+q7 = input("Jaký je objem válce?:")
+if q7 == "πr2 * v":
+    print("správně")
+
+q8 = input("Jaký je povrch válce?:")
+if q8 == "2πr(r+v)":
+    print("správně")
+else:
+    print("špatně")
+
+q9 = input("Jaký je objem kužele?:")
+if q9 == "1/3πr2 * v":
+    print("správně")
+else:
+    print("špatně")
+q9 = input("Jaký je povrch kužele?:")
+if q9 == "πr 2 + πrs":
+    print("správně")
+else:
+    print("špatně")
+q10 = input("Jaký je objem koule?:")
+if q10 == "3/4 πr3":
+    print("spravně")
+else:
+    print("špatně")
+
+
+
